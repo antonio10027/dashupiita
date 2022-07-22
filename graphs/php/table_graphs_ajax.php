4c5acb9141089dbbdf8da72cec9462071579bb57
antonio10027 <mcantoniog@gmail.com>
2022-07-22 17:48:51 -0500

    Prueba

---
+++ graphs/php/table_graphs_ajax.php
@@ -0,0 +1,45 @@
+<?php
+/*if(isset($_GET['elem'])&& isset($_GET['start']) && isset($_GET['end']) && isset($_GET['estac'])){
+   $elemento = $_GET['elem'];
+   $start = $_GET['start'];
+   $end = $_GET['end'];
+   $stac=$_GET['estac'];*/
+    //print_r($varRecibida);
+    
+    $serverName = "D4G79RJ3";
+    $connectionOptions = [
+      "Database"=>"contaminantes2",
+      "Uid"=>"contams",
+      "PWD"=>"contams"
+    ]; 
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    if($conn == false)
+      die(print_r(sqlsrv_errors(),true));
+    else  //echo "Connection Successful";
+    $inicio='2007-01-31';
+    $fin = '2019-12-31';
+    $idParam = 'o3';
+    $idStation='ACO';
+    /*$sql="SELECT date, nombre_estacion ,id_parameter ,value 
+    FROM view_avg_stations_WithAllData_Per_Month_2005_2021
+    where date BETWEEN '".$inicio."' AND '".$fin."' and id_parameter= '".$idParam."' and id_station = '".$idStation."' and estatus='ACTIVA'
+    group by date, nombre_estacion, id_parameter, value
+    ORDER BY date asc";*/
+
+    $sql="SELECT date, nombre_estacion ,id_parameter ,value 
+    FROM view_avg_stations_WithAllData_Per_Month_2005_2021
+    where date BETWEEN '2005-12-31' AND '2019-01-31' and id_parameter= 'o3' and id_station = 'ACO' and estatus='ACTIVA'
+    group by date, nombre_estacion, id_parameter, value
+    ORDER BY date asc";
+    $res = sqlsrv_query($conn, $sql);
+    $data = ''; 
+    
+    while($row = sqlsrv_fetch_array($res)){
+        $data = $row;
+        
+    }
+ echo json_encode($data);
+    
+ // }
+
+?>
---
+++ includes/js/js_inner.php
@@ -0,0 +1,34 @@
+
+<script src="../plugins/jquery/jquery.min.js"></script>
+<script src="../plugins/bootstrap/js/bootstrap.js"></script>
+<script src="../plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+<script src="../plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+<script src="../plugins/node-waves/waves.js"></script>
+<!-- Chart Plugins Js -->
+<script src="../plugins/chartjs/Chart.bundle.js"></script>
+
+
+<script src="../js/leafletFullscreen.js"></script>
+<script src="../js/admin.js"></script>
+<script src="../js/pages/charts/chartjs.js"></script>
+<script src="../js/pages/tables/jquery-datatable.js"></script>
+
+<!-- Autosize Plugin Js -->
+<script src="../plugins/autosize/autosize.js"></script>
+ <!-- Moment Plugin Js -->
+ <script src="../plugins/momentjs/moment.js"></script>
+<!-- Bootstrap Material Datetime Picker Plugin Js -->
+<script src="../plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js"></script>
+<script src="../js/pages/forms/basic-form-elements.js"></script>
+<script src="../js/demo.js"></script>
+<!-- Jquery DataTable Plugin Js -->
+<script src="../plugins/jquery-datatable/jquery.dataTables.js"></script>
+<script src="../plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
+<script src="../plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
+<script src="../plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
+<script src="../plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
+<script src="../plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
+<script src="../plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
+<script src="../plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
+<script src="../plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
